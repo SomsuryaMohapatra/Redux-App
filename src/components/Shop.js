91b10447424716a622474d1e2e3c3d64dd89d952
Somsurya Mohapatra <31140643+SomsuryaMohapatra@users.noreply.github.com>
2023-07-09 17:44:43 +0530

    Updating state from a different component in redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,49 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/actionCreator";
 
 export default function Shop() {
+  const dispatch = useDispatch();
+  const actions = bindActionCreators(actionCreators, dispatch);
   return (
     <div class="d-grid gap-2 col-3 mx-auto">
-      <h4>Deposit / Withdraw Money</h4>
-      <button class="btn btn-primary" type="button">
+      {/* <h4>Deposit / Withdraw Money</h4>
+      <button
+        class="btn btn-primary"
+        type="button"
+        onClick={() => {
+          dispatch(actionCreators.depositMoney(100));
+        }}
+      >
         Deposit
       </button>
-      <button class="btn btn-primary" type="button">
+      <button
+        class="btn btn-primary"
+        type="button"
+        onClick={() => {
+          dispatch(actionCreators.withdrawMoney(100));
+        }}
+      >
+        Withdraw
+      </button> */}
+      <h4>Deposit / Withdraw Money</h4>
+      <button
+        class="btn btn-primary"
+        type="button"
+        onClick={() => {
+          actions.depositMoney(100);
+        }}
+      >
+        Deposit
+      </button>
+      <button
+        class="btn btn-primary"
+        type="button"
+        onClick={() => {
+          actions.withdrawMoney(100);
+        }}
+      >
         Withdraw
       </button>
     </div>
